bc4d1e66c29b902cc784bd82705b7658b8044562
Christof <christof.ruch@gmx.de>
2025-03-09 21:26:17 +0100

    Starting a new run for the synth parameter exposure. This is supposed to be a single class/capability, with potentically more complex data. This is more suited to be exposed to the Python adaptations than the original design which was heavily split into too many classes.

--- base/include/DetailedParametersCapability.h
+++ base/include/DetailedParametersCapability.h
@@ -10,9 +10,44 @@
 
 namespace midikraft {
 
+	// Old class 
 	class DetailedParametersCapability {
 	public:
 		virtual std::vector<std::shared_ptr<SynthParameterDefinition>> allParameterDefinitions() const = 0;
+	};
+
+	enum ParamType {
+		VALUE = 0,
+		CHOICE = 1,
+		LIST = 2,
+	};
+
+	struct ParamDef {
+		int param_id;
+		std::string name;
+		ParamType param_type;
+		juce::var values;
+		std::optional<int> cc_number; // If a CC number is assigned to this parameter
+		std::optional<int> nrpn_number; // If an NRPN number is assigned to this parameter
+	};
+
+	struct ParamVal {
+		int param_id;
+		juce::var value;
+	};
+
+	// New class, all in one, for Python interface
+	class SynthParametersCapability {
+	public:
+		virtual std::vector<ParamDef> getParameterDefinitions() const = 0;
+
+		virtual std::vector<ParamVal> getParameterValues(std::shared_ptr<DataFile> const patch, bool onlyActive) const = 0;
+
+		// For clustering/auto-categorization and similarity search.
+		// This is allowed to drop out parameters not considered relevant, and should convert list parameters to 
+		// vector parameters. Must return always vectors of the same length.
+		virtual std::vector<float> createFeatureVector(std::shared_ptr<DataFile> const patch) const = 0;
+
 	};
 
 }
